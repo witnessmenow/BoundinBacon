16873c8842897ff49a994c7aa34c9d7bbe58ab4a
Brian Lough <ecenuig@gmail.com>
2014-07-22 21:08:28 +0100

    Creation of Pig object
    Mainly just a placeholder

---
+++ core/src/main/java/com/ladinc/boundingBacon/core/objects/Pig.java
@@ -0,0 +1,29 @@
+package com.ladinc.boundingBacon.core.objects;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Pig 
+{
+	public static float PIG_WIDTH;
+	public static float PIG_HEIGHT;
+	
+	private float power;
+	public int pigNumber;
+	
+	public Body body;
+	
+	
+	
+	public Pig(World world, int number, StartingPosition startingPos)
+	{
+		
+	}
+	
+	private void createBody(World world, StartingPosition startingPos)
+	{
+		
+	}
+	
+
+}
---
+++ core/src/main/java/com/ladinc/boundingBacon/core/objects/StartingPosition.java
@@ -0,0 +1,17 @@
+package com.ladinc.boundingBacon.core.objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class StartingPosition 
+{
+	public float angle;
+	public Vector2 position;
+	
+	public StartingPosition(Vector2 pos, float ang)
+	{
+		this.angle = ang;
+		this.position = pos;
+	}
+	
+
+}
